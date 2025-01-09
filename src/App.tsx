cada8266c929458ce15b3d201463669335a8fd33
Guillaume Fayard <guillaume.fayard@doctolib.com>
2025-01-09 10:43:30 +0100

    add formatter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
 function App() {
-  return <h1>Magic</h1>
+  return (
+    <>
+      <h1>Magic</h1>
+      <div>
+        <h2>Player 1</h2>
+        <div>Deck</div>
+        <div>Hand</div>
+      </div>
+      <div>
+        <h2>Battlefield</h2>
+      </div>
+      <div>
+        <h2>Player 2</h2>
+        <div>Deck</div>
+        <div>Hand</div>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
